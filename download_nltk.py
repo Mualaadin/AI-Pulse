272f76f7494ae8cf952fcb9e667228c444ddf336
Mualaadin <95096387+Mualaadin@users.noreply.github.com>
2025-09-27 10:49:29 +0300

    Add files via upload

---
+++ download_nltk.py
@@ -0,0 +1,11 @@
+import nltk
+
+# Download the wordnet dataset
+nltk.download('wordnet')
+
+# Download other commonly used datasets (recommended)
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+print("✅ NLTK datasets downloaded successfully!")
